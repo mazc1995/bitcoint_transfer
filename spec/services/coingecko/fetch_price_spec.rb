869846dde3d7d0871cc61a3663d1ab164511ac7b
MarioZ <mazc1995@gmail.com>
2025-06-12 21:09:01 -0500

    Seed database with initial User records for testing; create two users with specified balances

--- spec/services/coingecko/fetch_price_spec.rb
+++ spec/services/coingecko/fetch_price_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Coingecko::FetchPrice do
   describe '#call' do
     it 'returns the price of the from currency in the to currency' do
-      fetch_price = Coingecko::FetchPrice.new('bitcoin', 'usd')
+      fetch_price = Coingecko::FetchPrice.new
       fetch_price.call
       expect(fetch_price.result).to be_a(Float)
     end
