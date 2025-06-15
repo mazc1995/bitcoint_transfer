c00c520611416a1b4dc597a5dd6feb370466f93e
MarioZ <mazc1995@gmail.com>
2025-06-15 07:27:58 -0500

    Refactor btc_price method in CurrenciesController to use a local variable for better clarity and maintainability.

--- app/controllers/api/v1/currencies_controller.rb
+++ app/controllers/api/v1/currencies_controller.rb
@@ -3,8 +3,8 @@ module Api::V1
 
     # GET /api/v1/currencies/btc_price
     def btc_price
-      @btc_price = Coingecko::FetchPrice.new.call
-      render json: { usd_btc_price: @btc_price }, status: :ok
+      btc_price = Coingecko::FetchPrice.new.call
+      render json: { usd_btc_price: btc_price }, status: :ok
     rescue StandardError
       render json: { error: 'Error fetching BTC price' }, status: :bad_gateway
     end
